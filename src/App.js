ec9060d3b4869338b963cce3205421a3aacfce3f
Kisyok <kisoindran@gmail.com>
2022-02-19 14:23:10 +0800

    Todo comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import Todo from './components/Todo'
+
 function App() {
   return (
-    <div>Hello</div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="React 1" />
+      <Todo text="React 2" />
+      <Todo text="React 3" />
+    </div>
   );
 }
 
